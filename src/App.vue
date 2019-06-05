cceb65184fb240f03f3e230579d4683ac70a6df1
yanco3a <yanco3a@gmail.com>
2019-06-05 18:29:40 +0100

    Add styling and design to the web application - adds materialise, font-awesome and dependencies - update page HTML title
    solo: @yanco3a

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
+    <NavBar/>
+    <div class="container">
+      <router-view/>
     </div>
-    <router-view/>
   </div>
 </template>
+
+<script>
+import NavBar from './views/NavBar'
+export default {
+  name: 'app',
+  components: { NavBar }
+}
+</script>
