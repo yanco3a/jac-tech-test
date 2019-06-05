a5370a05eedb0b518eb0707aa9bcbb8b1a295376
yanco3a <yanco3a@gmail.com>
2019-06-05 19:08:53 +0100

    Pull jobs from firestore when the page is created
    solo: @yanco3a

--- src/components/firebaseInit.js
+++ src/components/firebaseInit.js
@@ -1,13 +1,12 @@
 // npm dependencies
-import firebase from 'firebase'
-import 'firebase/firestore'
+import firebase from "firebase/app";
+import "firebase/firestore";
 
 // local dependencies
-import firebaseConfig from './firebaseConfig'
+import firebaseConfig from "./firebaseConfig";
 
 // Constants
-const firebaseApp = firebase.initializeApp(firebaseConfig)
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 // initialise Firestore
-export default firebaseApp.firestore()
-
+export default firebaseApp.firestore();
--- src/views/JobBoard.vue
+++ src/views/JobBoard.vue
@@ -1,16 +1,49 @@
 <template>
     <div id="job-board">
-        <h3>Job Board</h3>
+        <ul class="collection with-header">
+            <li class="collection-header">
+                <h4>Job Board</h4>
+            </li>
+            <li v-for="job in jobs" v-bind:key="job.id" class="collection-item">
+                <div v-if="convertToDays(job.jobOpenDate) >= 0">
+                    <div>
+                        {{job.jobTitle}} | job opens in <div class="chip">{{ convertToDays(job.jobOpenDate) }} </div> days
+                    </div>
+                </div>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
+import db from "../components/firebaseInit";
+const moment = require('moment')
+
 export default {
     name: 'job-board',
     data: () => {
         return {
-
+            jobs: []
         }
+    },
+    methods: {
+        convertToDays: firestoreTime => {
+            const future = moment((firestoreTime).toDate())
+            const now = moment();
+            return future.diff(now, 'days')
+        }
+    },
+    created () {
+        db.collection('Jobs').get().then(snapshot => {
+            snapshot.forEach(doc => {
+                const data = {
+                    'id': doc.id,
+                    'jobOpenDate': doc.data().jobOpenDate,
+                    'jobTitle': doc.data().jobTitle
+                }
+                this.jobs.push(data)
+            })
+        })
     }
-};
+}
 </script>
